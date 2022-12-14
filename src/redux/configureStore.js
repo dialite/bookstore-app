afabd5a3810e452b98580957340d33bf390eefad
dialite <raymondolisa01@gmail.com>
2022-12-14 07:35:56 +0100

    Add reducers and action

---
+++ src/redux/configureStore.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import booksReducer from './books/books';
+import categoriesReducer from './categories/categories';
+
+const store = configureStore({
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
+  middleware: [thunk],
+});
+
+export default store;
